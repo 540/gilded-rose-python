a9e3acd9b67621ef4b1e6efc9d70f875662fb092
Gorka Moreno Asín <gorka@540deg.com>
2023-10-31 07:34:48 +0100

    Unify test assertions

--- tests/test_gilded_rose.py
+++ tests/test_gilded_rose.py
@@ -1,9 +1,10 @@
-from hamcrest import assert_that, equal_to, is_
+from hamcrest import assert_that, is_
 from src.gilded_rose import Item, GildedRose
+
 
 def test_foo():
     items = [Item("foo", 0, 0)]
     gilded_rose = GildedRose(items)
     gilded_rose.update_quality()
 
-    assert_that(items[0].name, is_(equal_to(3)))
+    assert_that(items[0].name, is_(3))

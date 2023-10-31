bf7a296c6b6e59a10b9608a46292e071e80de8cc
Javier Gutierrez <javier.gutierrez@540deg.com>
2023-10-31 15:11:03 +0100

    Fix README

--- tests/test_gilded_rose.py
+++ tests/test_gilded_rose.py
@@ -1,10 +1,10 @@
 from hamcrest import assert_that, is_
 from src.gilded_rose import Item, GildedRose
 
+class TestGildedRose:
+    def test_foo(self):
+        items = [Item("foo", 0, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
 
-def test_foo():
-    items = [Item("foo", 0, 0)]
-    gilded_rose = GildedRose(items)
-    gilded_rose.update_quality()
-
-    assert_that(items[0].name, is_(3))
+        assert_that(items[0].quality, is_(0))
